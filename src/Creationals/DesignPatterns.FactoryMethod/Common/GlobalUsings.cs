63a0eb936c3658eb926c6ad994aae36ef68329e4
Brendo Pereira Viegas <238365068+brendo-pereira-viegas@users.noreply.github.com>
2025-10-23 01:28:00 -0300

    Remove Application class and move payment execution

--- src/Creationals/DesignPatterns.FactoryMethod/Common/GlobalUsings.cs
+++ src/Creationals/DesignPatterns.FactoryMethod/Common/GlobalUsings.cs
@@ -1,5 +1,4 @@
-﻿global using DesignPatterns.FactoryMethod.Common;
-global using DesignPatterns.FactoryMethod.Enums;
+﻿global using DesignPatterns.FactoryMethod.Enums;
 global using DesignPatterns.FactoryMethod.Factories;
 global using DesignPatterns.FactoryMethod.Interfaces;
 global using DesignPatterns.FactoryMethod.Services;
--- src/Creationals/DesignPatterns.FactoryMethod/Program.cs
+++ src/Creationals/DesignPatterns.FactoryMethod/Program.cs
@@ -1,1 +1,5 @@
-﻿Application.Execute();
+﻿IPaymentMethod? debitCardPayment = PaymentMethodFactory.Create(PaymentType.DEBIT_CARD);
+IPaymentMethod? creditCardPayment = PaymentMethodFactory.Create(PaymentType.CREDIT_CARD);
+debitCardPayment?.Execute();
+creditCardPayment?.Execute();
+Console.ReadKey();
